33923b01003bd5d86350c68648760a3d781a5d72
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-05 19:43:51 +0530

    Refactor TryCatch middleware to include error handling and TypeScript type definitions

--- backend/user/dist/config/TryCatch.d.ts
+++ backend/user/dist/config/TryCatch.d.ts
@@ -1,2 +1,4 @@
-export {};
+import type { RequestHandler } from "express";
+declare const Trycatch: (handler: RequestHandler) => RequestHandler;
+export default Trycatch;
 //# sourceMappingURL=TryCatch.d.ts.map
--- backend/user/dist/config/TryCatch.js
+++ backend/user/dist/config/TryCatch.js
@@ -1,3 +1,12 @@
-import { RequestHandler } from "express";
-const Trycatch = (handler) => { };
+const Trycatch = (handler) => {
+    return async (req, res, next) => {
+        try {
+            await handler(req, res, next);
+        }
+        catch (error) {
+            next(error);
+        }
+    };
+};
+export default Trycatch;
 //# sourceMappingURL=TryCatch.js.map
