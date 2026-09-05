08cab04c66234ebc0bbc4df669c1526b98fbf51c
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-05 18:25:09 +0530

    Add RabbitMQ configuration and error handling in consumer

--- backend/mail/dist/consumer.js
+++ backend/mail/dist/consumer.js
@@ -4,8 +4,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const startSendOtpConsumer = async () => {
     try {
+        const { Rabbitmq_Host, Rabbitmq_Username, Rabbitmq_Password } = process.env;
+        if (!Rabbitmq_Host || !Rabbitmq_Username || !Rabbitmq_Password) {
+            throw new Error("Missing RabbitMQ environment variables");
+        }
         const connection = await amqp.connect({
             protocol: "amqp",
+            hostname: Rabbitmq_Host,
+            port: 5672,
+            username: Rabbitmq_Username,
+            password: Rabbitmq_Password,
         });
     }
     catch (error) {
--- backend/mail/src/consumer.ts
+++ backend/mail/src/consumer.ts
@@ -5,9 +5,17 @@ dotenv.config();
 
 export const startSendOtpConsumer = async()=>{
     try{
+        const { Rabbitmq_Host, Rabbitmq_Username, Rabbitmq_Password } = process.env;
+
+        if (!Rabbitmq_Host || !Rabbitmq_Username || !Rabbitmq_Password) {
+            throw new Error("Missing RabbitMQ environment variables");
+        }
         const connection = await amqp.connect({
             protocol: "amqp",
-            
+            hostname: Rabbitmq_Host,
+            port: 5672,
+            username: Rabbitmq_Username,
+            password: Rabbitmq_Password,
         })
     } catch(error){
         console.log("Failed to start rabbitmq consumer", error);
