817a1757090b8c487f68571376516e17e7d96613
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-05 15:04:45 +0530

    Added redis to project and configured to run it throuly

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,12 +1,26 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDb from './config/db.js'
+import { createClient } from 'redis'    
+import userRoutes from './routes/user.js'
 
 dotenv.config()
 
 connectDb()
 
+const redisUrl = process.env.REDIS_URL
+if (!redisUrl) {
+  throw new Error('REDIS_URL is not defined in .env')
+}
+export const redisClient = createClient({
+  url: redisUrl,
+})
+
+redisClient.connect().then(()=>console.log("Connected to Redis")) .catch(console.error)
+
 const app = express()
+
+app.use("api/v1", userRoutes)
 
 const port = process.env.PORT
 
