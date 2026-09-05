d8b5db02100b5724fd8ec821060717140ce4b111
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-05 18:20:16 +0530

    Implement OTP consumer functionality with RabbitMQ connection and error handling

--- backend/mail/src/consumer.ts
+++ backend/mail/src/consumer.ts
@@ -1,3 +1,15 @@
 import amqp from 'amqplib';
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+dotenv.config();
+
+export const startSendOtpConsumer = async()=>{
+    try{
+        const connection = await amqp.connect({
+            protocol: "amqp",
+            
+        })
+    } catch(error){
+        console.log("Failed to start rabbitmq consumer", error);
+    }
+}
