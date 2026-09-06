d98c17177acd515b82af0cdf228e8af73be5b4f0
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-06 14:41:05 +0530

    Add rate limiting and OTP sending functionality to loginUser

--- backend/user/dist/controllers/user.js
+++ backend/user/dist/controllers/user.js
@@ -1,8 +1,25 @@
+import { publishToQueue } from "../config/rabbitmq.js";
 import TryCatch from "../config/TryCatch.js";
 import { redisClient } from "../index.js";
 export const loginUser = TryCatch(async (req, res) => {
     const { email } = req.body;
     const rateLimitKey = `otp:ratelimit:${email}`;
     const rateLimit = await redisClient.get(rateLimitKey);
+    if (rateLimit) {
+        res.status(429).json({ message: "Too many requests, please try again later"
+        });
+        return;
+    }
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    const otpKey = `otp:${email}`;
+    await redisClient.set(otpKey, otp, { EX: 300 });
+    // res.json({message: "OTP sent successfully", otp})
+    await redisClient.set(rateLimitKey, "true", { EX: 60 });
+    const message = {
+        to: email,
+        subject: "Your OTP code",
+        text: `Your OTP is ${otp}. It will expire in 5 minutes.`,
+    };
+    await publishToQueue("send-otp", message);
 });
 //# sourceMappingURL=user.js.map
