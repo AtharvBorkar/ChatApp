2114af007bc4af1f83963ad8821722abac61aa06
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-06 15:58:20 +0530

    refactor: comment out token generation in verifyUser function

--- backend/user/dist/controllers/user.js
+++ backend/user/dist/controllers/user.js
@@ -50,6 +50,6 @@ export const verifyUser = TryCatch(async (req, res) => {
         const name = email.slice(0, 8);
         user = await User.create({ name, email });
     }
-    const token = user.generateToken();
+    // const token = user.generateToken()
 });
 //# sourceMappingURL=user.js.map
