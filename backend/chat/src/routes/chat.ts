2439a9df3fed9d17b2ad1b7b7291e2157d277191
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-08 02:52:02 +0530

    feat: add image upload functionality to message route using multer middleware

--- backend/chat/src/routes/chat.ts
+++ backend/chat/src/routes/chat.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { creatNewChat, getAllChats } from '../controllers/chat.js';
+import upload from '../middlewares/multer.js';
+import { creatNewChat, getAllChats, sendMessage } from '../controllers/chat.js';
 import { isAuth } from '../middlewares/isAuth.js';
 
 const router = express.Router();
 
 router.post("/chat/new", isAuth, creatNewChat)
 router.get("/chat/all", isAuth, getAllChats)
+router.post("/message", isAuth, upload.single("image"), sendMessage)
 
 export default router
