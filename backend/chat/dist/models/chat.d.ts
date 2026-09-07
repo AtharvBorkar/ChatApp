e463be424aefcf2620c44b6f26d8694dc170892d
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-07 18:03:06 +0530

    feat: enhance chat model with TypeScript definitions and schema implementation

--- backend/chat/dist/models/chat.d.ts
+++ backend/chat/dist/models/chat.d.ts
@@ -1,2 +1,11 @@
-export {};
+import { Document } from 'mongoose';
+export interface IChat extends Document {
+    users: string[];
+    latestMessage: {
+        text: string;
+        sender: string;
+    };
+    createdAt: Date;
+    updatedAt: Date;
+}
 //# sourceMappingURL=chat.d.ts.map
--- backend/chat/dist/models/chat.js
+++ backend/chat/dist/models/chat.js
@@ -1,2 +1,11 @@
-import mongooge from 'mongoose';
+import mongooge, { Document, Schema } from 'mongoose';
+const schema = new Schema({
+    users: [{ type: String, required: true }],
+    latestMessage: {
+        text: String,
+        sender: String,
+    }
+}, {
+    timestamps: true,
+});
 //# sourceMappingURL=chat.js.map
