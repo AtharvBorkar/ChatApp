b9e29eef6a3e0389697df8b6de9efd784bab5413
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-08 02:43:10 +0530

    Refactor code structure for improved readability and maintainability

---
+++ backend/chat/src/middlewares/multer.ts
@@ -0,0 +1,12 @@
+import multer from "multer"
+import {CloudinaryStorage} from "multer-storage-cloudinary"
+import cloudinary from "../config/cloudinary.js"
+
+const storage  = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params:{
+        folder:"chat-images",
+        allowed_formats: ["jpg", "jpeg", "png", "gif", "webp"],
+        transforamations: [{width: 800, height: 600, crop: "limit"}],
+    }
+})
