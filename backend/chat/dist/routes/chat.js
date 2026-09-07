0dc35d12e9c0840cff610e74e7aa232f73a5d8a0
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-07 21:03:15 +0530

    feat: add new chat route with authentication middleware

--- backend/chat/dist/routes/chat.js
+++ backend/chat/dist/routes/chat.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import { creatNewChat } from '../controllers/chat.js';
+import { isAuth } from '../middlewares/isAuth.js';
 const router = express.Router();
+router.post("/chat/new", isAuth, creatNewChat);
 export default router;
 //# sourceMappingURL=chat.js.map
--- backend/chat/src/routes/chat.ts
+++ backend/chat/src/routes/chat.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import { creatNewChat } from '../controllers/chat.js';
+import { isAuth } from '../middlewares/isAuth.js';
 
 const router = express.Router();
+
+router.post("/chat/new", isAuth, creatNewChat)
 
 export default router
