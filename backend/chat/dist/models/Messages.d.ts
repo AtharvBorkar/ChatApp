f38c23aa2ae41dd4be1fb08b386a9750e39e2089
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-07 23:26:26 +0530

    feat: enhance IMessage interface with chatId, sender, text, and image properties

--- backend/chat/dist/models/Messages.d.ts
+++ backend/chat/dist/models/Messages.d.ts
@@ -1,4 +1,11 @@
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 export interface IMessage extends Document {
+    chatId: Types.ObjectId;
+    sender: string;
+    text: string;
+    image?: {
+        url: string;
+        publicId: string;
+    };
 }
 //# sourceMappingURL=Messages.d.ts.map
--- backend/chat/src/models/Messages.ts
+++ backend/chat/src/models/Messages.ts
@@ -1,5 +1,13 @@
 import mongoose, {Document, Schema, Types} from 'mongoose';
 
 export interface IMessage extends Document{
-    
+    chatId: Types.ObjectId
+    sender: string
+    text: string
+    // createdAt: Date
+    // updatedAt: Date
+    image?: {
+        url: string
+        publicId: string
+    }
 }
