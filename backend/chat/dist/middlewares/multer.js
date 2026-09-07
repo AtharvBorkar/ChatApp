4598265fc157a86f52dff0449014af3a0ed38378
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-08 02:43:46 +0530

    refactor: remove unused Cloudinary storage configuration from multer middleware

--- backend/chat/dist/middlewares/multer.js
+++ backend/chat/dist/middlewares/multer.js
@@ -1,11 +1,2 @@
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary.js";
-const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: "chat-images",
-        allowed_formats: ["jpg", "jpeg", "png", "gif", "webp"],
-        transforamations: [{ width: 800, height: 600, crop: "limit" }],
-    }
-});
+export {};
 //# sourceMappingURL=multer.js.map
