120b1b09d869702b497efa23809477eca4c5b3ed
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-08 01:36:21 +0530

    feat: add getAllChats route to chat router

--- backend/chat/dist/routes/chat.js
+++ backend/chat/dist/routes/chat.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { creatNewChat } from '../controllers/chat.js';
+import { creatNewChat, getAllChats } from '../controllers/chat.js';
 import { isAuth } from '../middlewares/isAuth.js';
 const router = express.Router();
 router.post("/chat/new", isAuth, creatNewChat);
+router.get("/chat/all", isAuth, getAllChats);
 export default router;
 //# sourceMappingURL=chat.js.map
--- backend/chat/src/routes/chat.ts
+++ backend/chat/src/routes/chat.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { creatNewChat } from '../controllers/chat.js';
+import { creatNewChat, getAllChats } from '../controllers/chat.js';
 import { isAuth } from '../middlewares/isAuth.js';
 
 const router = express.Router();
 
 router.post("/chat/new", isAuth, creatNewChat)
+router.get("/chat/all", isAuth, getAllChats)
 
 export default router
