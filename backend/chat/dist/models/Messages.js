63080c00dd2eb0d089e0b12c2d3c25486704aa25
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-08 00:10:59 +0530

    feat: implement IMessage schema with properties for chatId, sender, text, image, messageType, seen, and seenAt

--- backend/chat/dist/models/Messages.js
+++ backend/chat/dist/models/Messages.js
@@ -1,2 +1,31 @@
-export {};
+import { Schema } from 'mongoose';
+const schem = new Schema({
+    chatId: {
+        type: Schema.Types.ObjectId,
+        ref: "Chat",
+        required: true
+    },
+    sender: {
+        type: String,
+        required: true
+    },
+    text: String,
+    image: {
+        url: String,
+        publicId: String
+    },
+    messageType: {
+        type: String,
+        enum: ["text", "image"],
+        default: "text"
+    },
+    seen: {
+        type: Boolean,
+        default: false
+    },
+    seenAt: {
+        type: Date,
+        default: null
+    }
+});
 //# sourceMappingURL=Messages.js.map
--- backend/chat/src/models/Messages.ts
+++ backend/chat/src/models/Messages.ts
@@ -15,3 +15,33 @@ export interface IMessage extends Document{
     createdAt: Date
     updatedAt: Date
 }
+
+const schem = new Schema<IMessage>({
+    chatId: {
+        type: Schema.Types.ObjectId,
+        ref: "Chat",
+        required: true
+    },
+    sender:{
+        type: String,
+        required: true
+    },
+    text: String,
+    image: {
+        url: String,
+        publicId: String
+    },
+    messageType: {
+        type: String,
+        enum: ["text", "image"],
+        default: "text"
+    },
+    seen:{
+        type: Boolean,
+        default: false
+    },
+    seenAt:{
+        type: Date,
+        default: null
+    }
+})
