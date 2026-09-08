c1b26be89a88d947cfecb5efd17618e0717debbe
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-09 04:01:13 +0530

    feat: add loading indicator and disable button during OTP sending on login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react'
-import { ArrowBigRight, ArrowRight, Mail } from 'lucide-react'
+import { ArrowBigRight, ArrowRight, Loader2, Mail } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import axios from 'axios'
 
@@ -47,11 +47,21 @@ const LoginPage = () => {
                     <label htmlFor="email" className="block text-sm font-medium text-gray-300 mb-2">Email Adress</label>
                     <input type="email" id="email" name="email" className="w-full px-4 py-2 rounded-lg bg-gray-700 border border-gray-600 text-white focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                 </div>
-                <button type="submit" className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed">
-                    <div className="flex items-center justify-center gap-2">
-                        <span>Send Verification Code</span>
-                        <ArrowRight className="w-5 h-5" />
-                    </div>
+                <button type="submit" className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed" disabled={loading}>
+                    {
+                        loading? (
+                        <div className="flex items-center justify-center gap-2">
+                            <Loader2 className="w-5 h-5" />
+                            Sending Opt to your mail...
+                        </div>
+                        ) : (
+                            <div className="flex items-center justify-center gap-2">
+                                <span>Send Verification Code</span>
+                                <ArrowRight className="w-5 h-5" />
+                            </div>
+                        )
+                    }
+                    
                 </button>
                 </form>
             </div>
