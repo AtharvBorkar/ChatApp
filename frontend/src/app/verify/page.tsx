43c97ca63edaba8032adb880528a49a428aa8ed7
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-09 04:25:54 +0530

    feat: enhance email verification page with OTP state management and loading indicators

--- frontend/src/app/verify/page.tsx
+++ frontend/src/app/verify/page.tsx
@@ -1,10 +1,17 @@
 "use client"
 import { ArrowRight, Loader2, Lock } from 'lucide-react'
-import { useSearchParams } from 'next/navigation'
-import React, { useState } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import React, { useRef, useState } from 'react'
 
 const VerifyPage = () => {
     const [loading, setLoading] = useState<boolean>(false)
+    const [otp, setOtp] = useState<string[]>(["", "", "", "", "", ""])
+    const error = useState<string>("")
+    const [resendLoading, setResendLoading] = useState<boolean>(false)
+    const [timer, setTimer] = useState<number>(60)
+    const inputRefs = useRef<Array<HTMLInputElement>|null>([])
+    const router = useRouter()
+
     const searchParams = useSearchParams()
     const email: string = searchParams.get('email') || ''
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
@@ -34,7 +41,7 @@ const VerifyPage = () => {
                     <label htmlFor="email" className="block text-sm font-medium text-gray-300 mb-2">Email Adress</label>
                     <input type="email" id="email" name="email" className="w-full px-4 py-2 rounded-lg bg-gray-700 border border-gray-600 text-white focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your email" /*value={email} onChange={(e) => setEmail(e.target.value)}*/ required/>
                 </div>
-                <button type="submit" className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed" /*disabled={loading}*/>
+                <button type="submit" className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed" disabled={loading}>
                     {
                         loading? (
                         <div className="flex items-center justify-center gap-2">
