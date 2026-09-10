2ee92d61e0613384d9ba50f9c42cac4daf8b58d1
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-10 21:45:14 +0530

    feat: refactor ChatSidebar component layout and improve button functionality

--- frontend/src/components/ChatSidebar.tsx
+++ frontend/src/components/ChatSidebar.tsx
@@ -1,5 +1,6 @@
 import { User } from '@/context/AppContext'
 import React, { useState } from 'react'
+
 import { X } from 'lucide-react'
 interface ChatSidebarProps {
     sidebarOpen: boolean
@@ -19,10 +20,16 @@ const ChatSidebar = ({sidebarOpen, setSidebarOpen, showAllUsers, setShowAllUsers
   return (
     <aside className={`fixed z-20 sm:static top-0 left-0 h-screen w-80 bg-gray-900 border-r boredr-gray-700 transform ${sidebarOpen ? "translate-x-0" : "-translate-x-full"} sm:translate-x-0 transition-transform duration-300 flex-col`}>
         {/*header*/}
-        <div className="sm:hidden flex justify-end mb-0">
-            <button className="">
-                <X className="w-5 h-5 text-gray-300"
-            </button>
+        <div className="p-6 border-gray-700">
+            <div className="sm:hidden flex justify-end mb-0">
+                <button onClick={()=>setSidebarOpen(false)} className="p-2 hover:bg-gray-700 rounded-lg transition-colors">
+                    <X className="w-5 h-5 text-gray-300"/>
+                </button>
+            </div>
+
+            <div className="flex items-center justify-between mb-6">
+
+            </div>
         </div>
     </aside>
   )
