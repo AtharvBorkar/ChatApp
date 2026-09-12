8c51a602da3251e3e57c1e5f3fa49c45315ed43f
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-12 12:56:01 +0530

    feat: add no conversation message in ChatSidebar when no chats are available

--- frontend/src/components/ChatSidebar.tsx
+++ frontend/src/components/ChatSidebar.tsx
@@ -141,7 +141,14 @@ const ChatSidebar = ({sidebarOpen, setSidebarOpen, showAllUsers, setShowAllUsers
                     }
                 </div>
             ) : (
-                <div></div>
+                <div className="flex flex-col items-center justify-center h-full text-center">
+                    <div className="p-4 bg-gray-400 rounded-full mb-4">
+                        <MessageCircle className="w-8 h-8 text-gray-400" />
+                    </div>
+                    <p className="text-gray-400 font-medium">No Conversation yet</p>
+                    <p className="text-sm text-gray-500 mt-1">Start a new conversation</p>
+                </div>
+
             )
             }
         </div>
