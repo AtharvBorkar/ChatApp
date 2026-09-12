0556e1f1fa46d669123eedecd72eb6ec193dd3aa
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-12 20:36:13 +0530

    feat: update ChatMessages component to render messages and handle user selection

--- frontend/src/components/ChatMessages.tsx
+++ frontend/src/components/ChatMessages.tsx
@@ -30,7 +30,34 @@ const ChatMessages = ( { selectedUser, messages, loggedInUser }: ChatMessagesPro
     }, [selectedUser, uniqueMessages]);
 
   return (
-    <div>ChatMessages</div>
+    <div className="flex-1 overflow-hidden">
+        <div className="h-full max-h-[calc(100vh-215px)] overflow-y-auto p-2 space-y-2 custom-scroll">
+            {
+                !selectedUser? (
+                    <p className="text-gray-400 text-center mt-20">
+                        Select a user to start chatting📩
+                    </p>
+                ) : (
+                    <>
+                    {
+                        uniqueMessages?.map((e,i)=>{
+                            const isSentByMe = e.sender === loggedInUser?._id
+                            const uniqueKey = `${e._id}-${i}`;
+                            
+                            return(
+                                <div className={`flex flex-col gap-1 mt-2 ${isSentByMe? "items-end" : "items-start"}`} >
+                                    <div className={`rounded-lg p-3 max-w-sm ${isSentByMe? "bg-blue-600 text-white" : "bg-gray-700 text-white"}`} >
+
+                                    </div>
+                                </div>
+                            )// Create a unique key using message ID and index
+                        })
+                    }
+                    </>
+                )
+            }
+        </div>
+    </div>
   )
 }
 
