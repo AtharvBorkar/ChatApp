c9c52c04ae826972fb01e1fdb9d572f7116e507c
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-13 21:32:04 +0530

    fix: replace app.listen with server.listen for WebSocket integration

--- backend/chat/dist/index.js
+++ backend/chat/dist/index.js
@@ -3,14 +3,15 @@ import dotenv from 'dotenv';
 import connectDb from './config/db.js';
 import chatRoutes from './routes/chat.js';
 import cores from 'cors';
+import { app, server } from './config/socket.js';
 dotenv.config();
 connectDb();
-const app = express();
+//const app = express();
 app.use(express.json());
 app.use(cores());
 app.use("/api/v1", chatRoutes);
 const port = process.env.PORT;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 //# sourceMappingURL=index.js.map
--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -3,12 +3,13 @@ import dotenv from 'dotenv';
 import connectDb from './config/db.js';
 import chatRoutes from './routes/chat.js';
 import cores from 'cors'
+import { app, server } from './config/socket.js';
 
 dotenv.config()
 
 connectDb()
 
-const app = express();
+//const app = express();
 
 app.use(express.json())
 
@@ -18,6 +19,6 @@ app.use("/api/v1", chatRoutes)
 
 const port = process.env.PORT
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
